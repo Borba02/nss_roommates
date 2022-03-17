9c9e0a6f1d8c0afc8deac381d873015330d715cb
Bryce Borba <borbaesb@gmail.com>
2022-03-17 13:24:12 -0700

    First chapter completed

---
+++ Roommates/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using Roommates.Repositories;
+using Roommates.Models;
+
+namespace Roommates
+{
+    class Program
+    {
+        //  This is the address of the database.
+        //  We define it here as a constant since it will never change.
+        private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true;TrustServerCertificate=true;";
+
+        static void Main(string[] args)
+        {
+            ChoreRepository choreRepo = new ChoreRepository(CONNECTION_STRING);
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+            RoommateRepository roommateRepo = new RoommateRepository(CONNECTION_STRING);
+            
+            bool runProgram = true;
+            while (runProgram)
+            {
+                string selection = GetMenuSelection();
+
+                switch (selection)
+                {
+                    case ("Show all rooms"):
+                        List<Room> rooms = roomRepo.GetAll();
+                        foreach (Room r in rooms)
+                        {
+                            Console.WriteLine($"{r.Name} has an Id of {r.Id} and a max occupancy of {r.MaxOccupancy}");
+                        }
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Search for room"):
+                        Console.Write("Room Id: ");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Room room = roomRepo.GetById(id);
+
+                        Console.WriteLine($"{room.Id} - {room.Name} Max Occupancy({room.MaxOccupancy})");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Add a room"):
+                        Console.Write("Room name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Max occupancy: ");
+                        int max = int.Parse(Console.ReadLine());
+
+                        Room roomToAdd = new Room()
+                        {
+                            Name = name,
+                            MaxOccupancy = max
+                        };
+
+                        roomRepo.Insert(roomToAdd);
+
+                        Console.WriteLine($"{roomToAdd.Name} has been added and assigned an Id of {roomToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Show all chores"):
+                        List<Chore> chores = choreRepo.GetAll();
+                        foreach (Chore ch in chores)
+                        {
+                            Console.WriteLine($"{ch.Name}");
+                        };
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Search for a chore"):
+                        Console.Write("Chore Id: ");
+                        int cId = int.Parse(Console.ReadLine());
+
+                        Chore chore = choreRepo.GetById(cId);
+
+                        Console.WriteLine($"{chore.Id} - {chore.Name}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Add a chore"):
+                        Console.Write("Chore name: ");
+                        string chName = Console.ReadLine();
+
+                        Chore choreToAdd = new Chore()
+                        {
+                            Name = chName
+                        };
+
+                        choreRepo.Insert(choreToAdd);
+
+                        Console.WriteLine($"{choreToAdd.Name} has been added and assigned an Id of {choreToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Search for a roommate"):
+                        Console.Write("Roommate Id: ");
+                        int rmId = int.Parse(Console.ReadLine());
+
+                        Roommate roommate = roommateRepo.GetById(rmId);
+
+                        Console.WriteLine($"{roommate.FirstName} in the {roommate.Room.Name} pays {roommate.RentPortion}%");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Show unassigned chores"):
+                        List<Chore> unassignedChores = choreRepo.GetUnassignedChores();
+                        foreach (Chore ch in unassignedChores)
+                        {
+                            Console.WriteLine($"{ch.Name} has yet to be assigned! Get to work!");
+                        }
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Assign chore to roommate"):
+                        List<Chore> listAllChores = choreRepo.GetAll();
+                        foreach (Chore ch in listAllChores)
+                        {
+                            Console.WriteLine($"{ch.Name} has an Id of {ch.Id}");
+                        }
+
+                        Console.WriteLine();
+                        Console.Write("Please select a chore by Id: ");
+                        int selectedChoreId = int.Parse(Console.ReadLine());
+
+                        List<Roommate> allRoommates = roommateRepo.GetAll();
+                        foreach (Roommate r in allRoommates)
+                        {
+                            Console.WriteLine($"{r.FirstName} {r.LastName} has an Id of {r.Id}");
+                        }
+
+                        Console.WriteLine();
+                        Console.Write("Please select a roommate by Id: ");
+                        int selectedRoommateId = int.Parse(Console.ReadLine());
+
+                        choreRepo.AssignChore(selectedRoommateId, selectedChoreId);
+                        Console.WriteLine("Get to Work!");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+
+                    case ("Exit"):
+                        runProgram = false;
+                        break;
+                }
+            }
+
+        }
+
+        static string GetMenuSelection()
+        {
+            Console.Clear();
+
+            List<string> options = new List<string>()
+            {
+                "Show all rooms",
+                "Search for room",
+                "Add a room",
+                "Show all chores",
+                "Search for a chore",
+                "Add a chore",
+                "Search for a roommate",
+                "Show unassigned chores",
+                "Assign chore to roommate",
+                "Exit"
+            };
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {options[i]}");
+            }
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine();
+                    Console.Write("Select an option > ");
+
+                    string input = Console.ReadLine();
+                    int index = int.Parse(input) - 1;
+                    return options[index];
+                }
+                catch (Exception)
+                {
+
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Roommates/Repositories/RoommateRepository.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT FirstName, LastName, RentPortion, ro.Name
+                                        From Roommate JOIN Room ro ON Roommate.RoomId = ro.Id
+                                        WHERE Roommate.Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate roommate = null;
+
+
+
+                        // If we only expect a single row back from the database, we don't need a while loop.
+                        if (reader.Read())
+                        {
+
+                            roommate = new Roommate
+
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                Room = new Room()
+                                {
+                                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                                }
+
+                            };
+                        }
+                        return roommate;
+                    }
+
+                }
+            }
+        }
+        public List<Roommate> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Roommate";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Roommate> roommates = new List<Roommate>();
+
+                        while (reader.Read())
+                        {
+                            Roommate roommate = new Roommate()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                                MovedInDate = reader.GetDateTime(reader.GetOrdinal("MoveInDate")),
+
+                            };
+                            roommates.Add(roommate);
+                        }
+                        return roommates;
+                    }
+
+                }
+            }
+        }
+        
+    }
+}
